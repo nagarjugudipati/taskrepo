3f230ead86eb8fc91ce8ae43400e1ee7904d4d46
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-09-14 10:57:12 +0530

    addding all

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class User
+    {
+        [Required]
+       public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPwd { get; set;}
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+    }
+}
